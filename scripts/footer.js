7ab2a88bdb08600fb19a9baa0c97785e4c18d1c1
Quails <centimofu@gmail.com>
2025-02-07 21:11:53 +0000

    :technologist: Version 0.2.0
    Introduces a script folder and adds a script to insert the footer on each page

---
+++ scripts/footer.js
@@ -0,0 +1,6 @@
+const footerHTML = '<footer>Version 0.2.0 🐾 Part of <a href="http://quailmail.moe" target="_blank">quailmail.moe</a></footer>';
+
+const footerElement = document.createElement('footer');
+footerElement.innerHTML = footerHTML;
+
+document.body.appendChild(footerElement);
